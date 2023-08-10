14c89776ccab3ac8c8fefdcb728662229330a167
Barni112 <nemeth.barnabas.4@gmail.com>
2023-08-10 14:19:00 +0200

    exceptionCatcher uses app.logger or mainLogger

--- src/server/middlewares/global/exception-handler.ts
+++ src/server/middlewares/global/exception-handler.ts
@@ -1,14 +1,16 @@
 import {NextFunction, Request, Response} from "express";
+import Application from "../../../application/application";
+import LoggerInterface from "../../../application/loggerInteface";
 
 
-export default function exceptionHandler() {
+export default function exceptionHandler(mainLogger: LoggerInterface) {
     return (req: Request, res: Response, next: NextFunction): void => {
         try {
             next();
         } catch (e) {
-            console.log("-------------------------------------");
-            console.log(e);
-            console.log("-------------------------------------");
+            const app: Application | undefined = req.context.get("app");
+            if (typeof app == "undefined") mainLogger.error(e);
+            else app.logger?.error(e);
             res.status(400).send(e);
         }
     };
