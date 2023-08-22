0bce492204f385c798b60c8f45f58dfec2ff4c85
Barni112 <nemeth.barnabas.4@gmail.com>
2023-08-22 10:17:12 +0200

    bug

--- application/authorizable.d.ts
+++ application/authorizable.d.ts
@@ -1,4 +1,6 @@
 export default interface Authorizable {
     id: number;
+    hasRole(roles: string[]): boolean;
+    getRoles(): string[];
     getUser(): any;
 }
--- src/application/authorizable.ts
+++ src/application/authorizable.ts
@@ -1,5 +1,9 @@
 export default interface Authorizable {
     id: number;
 
+    hasRole(roles: string[]): boolean;
+
+    getRoles(): string[];
+
     getUser(): any;
 }
