c3e2fa300825ee063fc521a685842dac0da32fee
Barni112 <nemeth.barnabas.4@gmail.com>
2023-08-31 10:52:04 +0200

    tsc

--- dist/graphane/server/middlewares/current-application-middleware.d.ts
+++ dist/graphane/server/middlewares/current-application-middleware.d.ts
@@ -1,3 +1,3 @@
 import { NextFunction, Request, Response } from "express";
 import { CurrentApplication } from "../../application/current-application";
-export declare function currentApplicationMiddleware(currentApplication: CurrentApplication): (req: Request, res: Response, next: NextFunction) => Promise<void>;
+export declare function currentApplicationMiddleware(currentApplication: CurrentApplication, fail?: boolean): (req: Request, res: Response, next: NextFunction) => Promise<void>;
--- dist/graphane/server/middlewares/current-application-middleware.js
+++ dist/graphane/server/middlewares/current-application-middleware.js
@@ -1,10 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.currentApplicationMiddleware = void 0;
-function currentApplicationMiddleware(currentApplication) {
+function currentApplicationMiddleware(currentApplication, fail = true) {
+    if (fail) {
+        return async (req, res, next) => {
+            await currentApplication.fetch(req);
+            currentApplication.fail.get(req);
+            next();
+        };
+    }
     return async (req, res, next) => {
         await currentApplication.fetch(req);
-        currentApplication.fail.get(req);
         next();
     };
 }
