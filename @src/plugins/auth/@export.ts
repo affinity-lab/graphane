474abd07d2fbfd6c359fab13bb27f7d8d49bae0b
Barni112 <nemeth.barnabas.4@gmail.com>
2023-08-31 12:08:45 +0200

    export fix

--- @src/plugins/auth/@export.ts
+++ @src/plugins/auth/@export.ts
@@ -3,6 +3,7 @@ import "./init-plugin";
 
 export * from "./authorizable";
 export * from "./current-authorized";
+export * from "./current-authorized-middleware";
 export * from "./permission-resolver";
 export * from "./auth-error";
 
