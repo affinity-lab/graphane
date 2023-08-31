041ba03a4ca357855d67754877ab3a7cd45c44bf
Barni112 <nemeth.barnabas.4@gmail.com>
2023-08-31 10:51:38 +0200

    currentApplicationMiddleware fail update

--- @src/graphane/server/middlewares/current-application-middleware.ts
+++ @src/graphane/server/middlewares/current-application-middleware.ts
@@ -2,10 +2,16 @@ import {NextFunction, Request, Response} from "express";
 import {CurrentApplication} from "../../application/current-application";
 
 
-export function currentApplicationMiddleware(currentApplication: CurrentApplication) {
+export function currentApplicationMiddleware(currentApplication: CurrentApplication, fail: boolean = true) {
+	if (fail) {
+		return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+			await currentApplication.fetch(req);
+			currentApplication.fail.get(req);
+			next();
+		};
+	}
 	return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 		await currentApplication.fetch(req);
-		currentApplication.fail.get(req);
 		next();
 	};
 }
